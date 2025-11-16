d510be6543c9ccaaf391d1fdda01a9b914f4055b
ishakcoban <ishakcoban99@outlook.com>
2025-11-16 15:01:06 +0300

    portfolio crud operations added

--- backend/src/portfolio/dto/create-portfolio.dto.ts
+++ backend/src/portfolio/dto/create-portfolio.dto.ts
@@ -1,3 +1,10 @@
+import { IsString, IsNotEmpty, MaxLength, MinLength, Matches } from 'class-validator';
+
 export class CreatePortfolioDto {
-    name:string
+  @IsString({ message: 'Name must be a string' })
+  @IsNotEmpty({ message: 'Name is required' })
+  @MinLength(3, { message: 'Name must be at least 3 characters' })
+  @MaxLength(50, { message: 'Name cannot exceed 50 characters' })
+  @Matches(/^[a-zA-Z0-9\s]+$/, { message: 'Name contains invalid characters' })
+  name: string;
 }
